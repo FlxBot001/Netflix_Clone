731995903957d3f6a3eb07bcb1686bc4639f746d
FlxBot001 <njugunafelix79@gmail.com>
2024-05-15 08:00:52 +0300

    Updates

--- src/Requests.js
+++ src/Requests.js
@@ -1,7 +1,7 @@
 const key = 'b7f30758daca7e45714b47c592d904b1'
 
 const requests = {
-    requestPopular: `https://api.themoviedb.org/3/tv/popular?api_key=${key}&language=en-US&page=1`,
+    requestPopular: `https://api.themoviedb.org/3/tv/popular?api_key=${key}&language=en-US&page=4`,
     requestTopRated: 'https://api.themoviedb.org/3/tv/top_rated?api_key=${key}&language=en-US&page=1',
     requestTrending: 'https://api.themoviedb.org/3/tv/on_the_air?api_key=${key}&language=en-US&page=1',
     requestUpComming: 'https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1',
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,6 +11,16 @@ const Main = () => {
             setMovies(response.data.results)
         })
     }, [])
+    //  console.log(movie);
+
+    const truncateString = (str, num) => {
+        if (str?.length > num) {
+            return str.slice(0, num) + '...';
+        }
+        else {
+            return str;
+        }
+        };
 
     useEffect(() => {
         if (movies.length > 0) {
@@ -26,9 +36,14 @@ const Main = () => {
                 {movie && (
                   <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
                 )}
-                <div>
-                  <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
-                  <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Watch Later</button>
+                <div className='absolute w-full top-[20%] p-4 md:p-8'>
+                    <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+                    <div className='my-4'>
+                        <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>Play</button>
+                        <button className='border text-white border-gray-300 py-2 px-5 ml-4'>Watch Later</button>
+                    </div>
+                    <p className='text-gay-300 text-sm'>Released: {movie?.release_date}</p>
+                    <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-300'>{truncateString(movie?.overview, 200)}</p>
                 </div>
             </div>
         </div>
