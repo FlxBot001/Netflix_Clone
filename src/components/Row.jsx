ce362985e43bd7e9ce83bb4ae97d89b7c174f2ae
FlxBot001 <njugunafelix79@gmail.com>
2024-05-15 13:10:48 +0300

    Errors in display of rows, check later

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { BiImageAlt } from 'react-icons/bi'
 
 const Row = ({ title, fetchURL}) => {
   const [movies, setMovies] = useState([])
@@ -17,7 +18,11 @@ const Row = ({ title, fetchURL}) => {
       <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
       <div className='relative flex items-center'>
         <div id={'slider'}>
-
+          {movies.map((item, id) => {
+            <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
+              <img src={`ttps://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
+            </div>
+          })}
         </div>
       </div>
     </>
