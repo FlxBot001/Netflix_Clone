03a0bfdf251f6406cc47ef0f5f55df7b66597ea7
FlxBot001 <njugunafelix79@gmail.com>
2024-05-15 12:38:38 +0300

    Update Row amnd Home components

--- src/Requests.js
+++ src/Requests.js
@@ -1,13 +1,11 @@
-const key = 'b7f30758daca7e45714b47c592d904b1'
+const key = 'b7f30758daca7e45714b47c592d904b1';
 
 const requests = {
-    requestPopular: `https://api.themoviedb.org/3/tv/popular?api_key=${key}&language=en-US&page=4`,
-    requestTopRated: 'https://api.themoviedb.org/3/tv/top_rated?api_key=${key}&language=en-US&page=1',
-    requestTrending: 'https://api.themoviedb.org/3/tv/on_the_air?api_key=${key}&language=en-US&page=1',
-    requestUpComming: 'https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1',
-    requestAiringToday: 'https://api.themoviedb.org/3/tv/airing_today?api_key=${key}&language=en-US&page=1',
-    requestAll: 'https://api.themoviedb.org/3/movie/movie_id/videos?api_key=${key}&language=en-US',
-    requestRecomended: 'https://api.themoviedb.org/3/movie/movie_id/recommendations?api_key=${key}&language=en-US&page=1',
-};
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
 
-export default requests
+  export default requests
